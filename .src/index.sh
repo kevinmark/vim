5691c0b8bab12ddb79b46ed0a89e6c43184edd47
kevinmark <ChangHungWork@gmail.com>
2013-11-14 11:13:18 +0800

    add new path for developing code : .src/

---
+++ .src/index.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# rm symbol files for analyzer
+if [ -f cscope.files ]; then
+	rm cscope.files
+	echo -e "rm cscope.files\n"
+fi
+if [ -f cscope.in.out ]; then
+	rm cscope.in.out
+	echo -e "rm cscope.in.out\n"
+fi
+if [ -f cscope.out ]; then
+	rm cscope.out
+	echo -e "rm cscope.out\n"
+fi
+if [ -f cscope.po.out ]; then
+	rm cscope.po.out
+	echo -e "rm cscope.po.out\n"
+fi
+if [ -f tags ]; then
+	rm tags
+	echo -e "rm tags\n"
+fi
+if [ -n "`ls -al | grep .cflow`" ]; then
+	rm *.cflow
+	echo -e "rm *.cflow\n"
+fi
+
+# argument "clean"
+if [  "$1" = "clean"  ]; then
+	exit 1
+fi
+
+echo -e "= establish symbol files =\n"
+find . -name "*.h" -o -name "*.c" -o -name "*.cc" > cscope.files
+sed -i 's#\.\/#'${PWD}'\/#g' cscope.files
+cscope -bkq -i cscope.files
+ctags -R
+
